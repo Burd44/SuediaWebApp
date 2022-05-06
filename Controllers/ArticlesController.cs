b4bcbc55b3df34601a507891ce6e1b46c5ef0ad6
Manolache Mihai <mihaimanolache03@gmail.com>
2022-05-06 12:00:58 +0300

    Fixed indentation

--- Controllers/ArticlesController.cs
+++ Controllers/ArticlesController.cs
@@ -1,14 +1,9 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SuediaWebApp.Data;
 using SuediaWebApp.Models;
-using Microsoft.AspNetCore.Authorization;
 
 namespace SuediaWebApp.Controllers
 {
