290731ed5bd3811651a79712889cac7d864c383f
Manolache Mihai <mihaimanolache03@gmail.com>
2022-05-08 17:15:22 +0300

    Added galleries

---
+++ Controllers/GalleriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuediaWebApp.Controllers
+{
+    public class GalleriesController : Controller
+    {
+        public IActionResult Gallery1()
+        {
+            return View();
+        }
+        public IActionResult Gallery2()
+        {
+            return View();
+        }
+        public IActionResult Gallery3()
+        {
+            return View();
+        }
+    }
+}
